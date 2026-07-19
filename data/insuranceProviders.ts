dab9476f00edd7f65429822cfa746512d9734efe
victor keya <keyavictor@gmail.com>
2026-07-19 11:32:45 +0200

    feat(data): add insurance provider configuration

---
+++ data/insuranceProviders.ts
@@ -0,0 +1,19 @@
+export const insuranceProviders = {
+  Kenya: [
+    "AAR Insurance",
+    "Jubilee Health Insurance",
+    "Britam",
+    "CIC Insurance",
+    "APA Insurance",
+    "Madison Insurance",
+    "Resolution Insurance",
+    "Old Mutual",
+    "Heritage Insurance",
+    "GA Insurance",
+    "First Assurance",
+    "ICEA LION",
+    "Liaison Group",
+    "Pacis Insurance",
+    "Kenya Alliance Insurance",
+  ],
+};
